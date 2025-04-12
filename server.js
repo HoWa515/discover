6c52a8dcbf30632401a3218d422c460fb15fe848
wh <sunfortea123@gmail.com>
2025-04-12 06:31:23 +0200

    new change

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const app = require('./app');
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`App runs on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
