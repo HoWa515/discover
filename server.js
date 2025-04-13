85057e5a56ce3514152dfe109824a8bf6887a22b
wh <sunfortea123@gmail.com>
2025-04-13 16:21:22 +0200

    change

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-// console.log(process.env.NODE_ENV);
+// con to atlas
+const DB = process.env.DATABASE;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('Atlas DB connected');
+  });
 //  start server
 
 const port = process.env.PORT || 8000;
