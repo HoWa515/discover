3d9a73133eb178e12e73e286a0467c642cdc0510
wh <sunfortea123@gmail.com>
2025-04-16 17:43:04 +0200

    new changes

---
+++ dev-data/data/importDevData.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('./../../models/tourModel');
+dotenv.config({ path: './config.env' });
+// con to atlas
+const DB = process.env.DATABASE;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('Atlas DB connected');
+  });
+
+// read json
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// delete all data from collection Tour
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  // run in cmd :  node dev-data/data/importDevData.js --delete
+  deleteData();
+}
