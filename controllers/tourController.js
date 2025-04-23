8d2bce3bb639427c2bcf1793634c594107a94c99
wh <sunfortea123@gmail.com>
2025-04-23 16:55:43 +0200

    m

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,12 +1,19 @@
 const Tour = require('./../models/tourModel');
 //  route handler
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     const queryObj = { ...req.query };
-    // exclude fields that are not in DB
+    // 1.1-- exclude fields that are not in DB
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach((el) => delete queryObj[el]);
-    // filtering for not equal
+    // 1.2--filter that handles not equal
     let queryString = JSON.stringify(queryObj);
     queryString = queryString.replace(
       /\b(gte|gt|lte|lt)\b/g,
@@ -14,6 +21,36 @@ exports.getAllTours = async (req, res) => {
     );
 
     let query = Tour.find(JSON.parse(queryString));
+
+    // 2--sorting
+    if (req.query.sort) {
+      console.log(req.query.sort);
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3--projecting the fields return from DB
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields); // projecting
+    } else {
+      query = query.select('-__v'); // by default,don't show __v property of MongoDB
+    }
+
+    // 4--pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 50;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page not exist.');
+    }
+
     // execute query
     const tours = await query;
 
@@ -26,7 +63,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err.message,
     });
   }
 };
--- models/tourModel.js
+++ models/tourModel.js
@@ -31,7 +31,7 @@ const tourSchema = mongoose.Schema({
     required: [true, 'A tour must have a cover image'],
   },
   images: [String],
-  createdAt: { type: Date, default: Date.now() },
+  createdAt: { type: Date, default: Date.now(), select: false },
   startDates: [Date],
 });
 
