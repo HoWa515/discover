06305241143bf772a05b6b7b6391ba8dde3ed85c
wh <sunfortea123@gmail.com>
2025-04-27 20:45:30 +0200

    m

--- app.js
+++ app.js
@@ -18,8 +18,12 @@ app.use((req, res, next) => {
   next();
 });
 
-// mount routers
+// routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
+app.all('*', (req, res, next) => {
+  res
+    .status(404)
+    .json({ status: 'fail', message: `Can't find ${req.originalUrl}` });
+});
 module.exports = app;
