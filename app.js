09b736d525cec303f4b160e977cc869fe654e399
wh <sunfortea123@gmail.com>
2025-04-29 19:34:56 +0200

    m

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
 // handle undefined routes
 app.all('*', (req, res, next) => {
   const err = new AppError(`Can't find ${req.originalUrl}`, 404);
