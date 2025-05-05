f58a630c09fa58eeaaa47e3dd3fb9497f72f2484
wh <sunfortea123@gmail.com>
2025-05-05 09:45:34 +0200

    m

--- models/userModel.js
+++ models/userModel.js
@@ -38,6 +38,11 @@ const userSchema = mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -54,6 +59,12 @@ userSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000; // make sure JWT token crated after password is changed
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  //this points to current query
+  this.find({ active: { $ne: false } });
   next();
 });
 
