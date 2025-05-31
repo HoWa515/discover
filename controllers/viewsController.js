943433040a9ac2a45a286c1e6ec65d88f5d55ac7
wh <sunfortea123@gmail.com>
2025-05-31 08:44:06 +0200

    m

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,17 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All tours' });
-};
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  // 2) build template
+
+  // 3)render template
+  res.status(200).render('overview', {
+    title: 'All tours',
+    tours,
+  });
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', { title: 'The Forest Hiker' });
